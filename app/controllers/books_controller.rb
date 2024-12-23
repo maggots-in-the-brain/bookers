129109f734f6dd63aa8e054e50f96ef91bc4f89a
TaroYamada <183961351+maggots-in-the-brain@users.noreply.github.com>
2024-12-23 03:52:04 +0000

    B

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def crette
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
