8d7e05e36b50a48eea2d0541a045f1e4ee72f371
TaroYamada <183961351+maggots-in-the-brain@users.noreply.github.com>
2024-12-26 02:51:33 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookers-leve|1.herokuapp.com' =>'homes#top'
+  get 'top' =>'homes#top'
   resources :books
   delete 'books/new' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
