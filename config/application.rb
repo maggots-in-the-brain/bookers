47153ef8cf7f345e0705cd2323d6d80eabc360ce
TaroYamada <183961351+maggots-in-the-brain@users.noreply.github.com>
2024-12-27 03:19:04 +0000

    課題：Bookersを作成しよう

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Bookers
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "bookers-level1.herokuapp.com"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
-  config.action_mailer.default_url_options = { host: 'bookers-level1.herokuapp.com' }
 end
